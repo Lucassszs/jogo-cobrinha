63d8ad7419f8fa40538fefdb54e096befaae11e3
Lucassszs <lipoilucaspepokemon@gmail.com>
2024-01-23 16:25:16 -0300

    alterações das mecanicas de movimentação e visuais
    E adicação do crescimento da cobra

--- main.py
+++ main.py
@@ -10,62 +10,103 @@ musica_de_fundo = pygame.mixer.music.load('02. Crazy Dave (Intro Theme).mp3')
 pygame.mixer.music.play(-1)
 
 barulho_colisao = pygame.mixer.Sound('sound-effect-file-ring-mp3.mp3')
-barulho_colisao.set_volume(0.1)
+barulho_colisao.set_volume(0.2)
 
 
 largura = 640
 altura = 480
-x = largura/2 
-y = altura/2
 
-x_azul = randint(40, 600)
-y_azul = randint(50, 430)
+x_cobra = int(largura/2) 
+y_cobra = int(altura/2)
 
-fonte = pygame.font.SysFont('arial', 40, True, True)
+velocidade = 10
+x_controle = velocidade
+y_controle = 0
+
+
+x_maca = randint(40, 600)
+y_maca = randint(50, 430)
+
 pontos = 0
-
+fonte = pygame.font.SysFont('arial', 40, bold=True, italic=True)
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo')
 relogio = pygame.time.Clock()
+lista_cobra = []
+comprimento_inicial = 5
+
+def aumenta_cobra(lista_cobra):
+    for XeY in lista_cobra:
+        #XeY = [x, y]
+        #XeY[0] = x
+        #XeY[1] = y
+
+        pygame.draw.rect(tela, (0,255,0), (XeY[0], XeY[1], 20, 20))
 
 while True:
     relogio.tick(30)
-    tela.fill((0,0,0))
+    tela.fill((255,255,255))
     
-    mensagem = f'pontos: {pontos}'
-    textoformatado = fonte.render(mensagem, True, (255, 255, 255))
+    mensagem = f'Pontos: {pontos}'
+    texto_formatado = fonte.render(mensagem, True, (0,0,0))
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-        '''
+        
         if event.type == KEYDOWN:
             if event.key == K_a:
-                x = x - 20
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle = -velocidade
+                    y_controle = 0
             if event.key == K_d:
-                x = x + 20
+                if x_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = velocidade
+                    y_controle = 0
             if event.key == K_w:
-                y = y - 20
+                if y_controle == velocidade:
+                    pass
+                else:
+                    y_controle = -velocidade
+                    x_controle = 0
             if event.key == K_s:
-                y = y + 20'''
-                        
-    if pygame.key.get_pressed()[K_a]:
-        x = x - 20
-    if pygame.key.get_pressed()[K_d]:
-        x = x + 20 
-    if pygame.key.get_pressed()[K_w]:
-        y = y - 20
-    if pygame.key.get_pressed()[K_s]:
-        y = y + 20
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    y_controle = velocidade
+                    x_controle = 0
+
+    x_cobra = x_cobra + x_controle
+    y_cobra = y_cobra + y_controle
         
-    ret_vermelho = pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
-    ret_azul = pygame.draw.rect(tela, (0,0,255), (x_azul,y_azul,40,50))
+    cobra = pygame.draw.rect(tela, (0,255,0), (x_cobra,y_cobra,20,20))
+    maca = pygame.draw.rect(tela, (255,0,0), (x_maca,y_maca,20,20))
     
-    if ret_vermelho.colliderect(ret_azul):
-        pontos = pontos + 1
-        x_azul = randint(40, 600)
-        y_azul = randint(50, 430)
+    if cobra.colliderect(maca):
+        x_maca = randint(40, 600)
+        y_maca = randint(50, 430)
+        pontos += 1
         barulho_colisao.play()
-    tela.blit(textoformatado, (400, 40))
+        comprimento_inicial = comprimento_inicial + 1
+
+    lista_cabeca = []
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+    
+    lista_cobra.append(lista_cabeca)
+
+    if len(lista_cobra) > comprimento_inicial:
+        del lista_cobra[0]
+
+    aumenta_cobra(lista_cobra)
+
+    tela.blit(texto_formatado, (450,40))
+
+    
     pygame.display.update()

b64505e7fb792aa73a4755f31dd563cb11ba48c9
Lucassszs <lipoilucaspepokemon@gmail.com>
2024-01-23 17:34:15 -0300

    projeto concluido, melhoras de gameplay, incluindo reset e melhora na movimentação da cobra

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ musica_de_fundo = pygame.mixer.music.load('02. Crazy Dave (Intro Theme).mp3')
 pygame.mixer.music.play(-1)
 
 barulho_colisao = pygame.mixer.Sound('sound-effect-file-ring-mp3.mp3')
-barulho_colisao.set_volume(0.2)
+barulho_colisao.set_volume(0.06)
 
 
 largura = 640
@@ -35,6 +35,7 @@ pygame.display.set_caption('Jogo')
 relogio = pygame.time.Clock()
 lista_cobra = []
 comprimento_inicial = 5
+morreu = False
 
 def aumenta_cobra(lista_cobra):
     for XeY in lista_cobra:
@@ -44,6 +45,20 @@ def aumenta_cobra(lista_cobra):
 
         pygame.draw.rect(tela, (0,255,0), (XeY[0], XeY[1], 20, 20))
 
+def reiniciar_jogo():
+    global pontos, comprimento_inicial, x_cobra, y_cobra, lista_cobra, lista_cabeca, x_maca, y_maca, morreu
+    pontos = 0
+    comprimento_inicial = 5
+    x_cobra = int(largura/2) 
+    y_cobra = int(altura/2)
+    lista_cobra = []
+    lista_cabeca = []
+    x_maca = randint(40, 600)
+    y_maca = randint(50, 430)
+    morreu = False
+    
+    
+    
 while True:
     relogio.tick(30)
     tela.fill((255,255,255))
@@ -88,25 +103,64 @@ while True:
     cobra = pygame.draw.rect(tela, (0,255,0), (x_cobra,y_cobra,20,20))
     maca = pygame.draw.rect(tela, (255,0,0), (x_maca,y_maca,20,20))
     
+    
+    
+    
+    
     if cobra.colliderect(maca):
         x_maca = randint(40, 600)
         y_maca = randint(50, 430)
         pontos += 1
         barulho_colisao.play()
-        comprimento_inicial = comprimento_inicial + 1
+        comprimento_inicial = comprimento_inicial + 5
 
     lista_cabeca = []
     lista_cabeca.append(x_cobra)
     lista_cabeca.append(y_cobra)
     
     lista_cobra.append(lista_cabeca)
-
+    
+    if lista_cobra.count(lista_cabeca) > 1:
+        fonte_morte = pygame.font.SysFont('arial', 20, bold=True, italic=True)
+        mensagem = 'Game Over! Pressione R para reiniciar'
+        texto_formatado = fonte_morte.render(mensagem, True, (0, 0, 0))
+        ret_texto = texto_formatado.get_rect()
+        morreu = True
+        
+        
+        while morreu:
+            tela.fill((255,255,255))
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_r:
+                        reiniciar_jogo()
+            
+            
+            ret_texto.center = (largura//2, altura//2)
+            tela.blit(texto_formatado, ret_texto)
+            pygame.display.update()
+    
+    
+    if x_cobra > largura:
+        x_cobra = 0
+    if x_cobra < 0:
+       x_cobra = largura    
+    if y_cobra < 0:
+        y_cobra = altura
+    if y_cobra > altura:
+       y_cobra = 0 
+       
+    
+            
     if len(lista_cobra) > comprimento_inicial:
         del lista_cobra[0]
 
     aumenta_cobra(lista_cobra)
 
-    tela.blit(texto_formatado, (450,40))
+    tela.blit(texto_formatado, (450, 40))
 
     
     pygame.display.update()
